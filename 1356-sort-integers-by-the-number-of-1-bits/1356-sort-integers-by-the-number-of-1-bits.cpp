8bf6cbefc3108e633a51734900f6b8c2de4b3373
Argha Ghosh <ghoshargha777@gmail.com>
2026-06-07 13:33:37 +0530

    Time: 1 ms (60.08%), Space: 14.1 MB (76.25%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    int countonebits( int num){
-
-        int count= 0;
-
-        while(num){
-            count += num&1;
-            num >>=1;
-        }
-        return count;
-    }
-
+   
 
 
     vector<int> sortByBits(vector<int>& arr) {
         
         auto lambda = [&] (int &a , int &b){
-            int count_a = countonebits(a);
-            int count_b = countonebits(b);
+            int count_a = __builtin_popcount(a);
+            int count_b = __builtin_popcount(b);
             if(count_a == count_b){
 
                  return a<b;
