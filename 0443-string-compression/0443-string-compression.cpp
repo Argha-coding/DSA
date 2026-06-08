3a4d1774e6e2f1fd3f319d3793ccd25ecf4a3f4e
Argha Ghosh <ghoshargha777@gmail.com>
2026-06-08 19:59:33 +0530

    Time: 0 ms (100%), Space: 13.9 MB (46.41%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int ans = 0;
+        
+        int n = chars.size();
+        int index= 0;
+        int i = 0;
+        while( i< n){
 
-        // iterate through input vector using i pointer
-        for (int i = 0; i < chars.size();) {
-            const char letter = chars[i]; // current character being compressed
-            int count = 0; // count of consecutive occurrences of letter
+            char curr_char = chars[i];
+            int count = 0;
+            while( i < n && chars[i] == curr_char){
 
-            // count consecutive occurrences of letter in input vector
-            while (i < chars.size() && chars[i] == letter) {
-                ++count;
-                ++i;
+                count++;
+                i++;
             }
 
-            // write letter to compressed vector
-            chars[ans++] = letter;
+            chars[index] = curr_char;
+            index++;
+            if(count > 1){
+                string count_str = to_string(count);
+                for( char &ch : count_str){
 
-            // if count is greater than 1, write count as string to compressed vector
-            if (count > 1) {
-                // convert count to string and iterate over each character in string
-                for (const char c : to_string(count)) {
-                    chars[ans++] = c;
+                    chars[index]= ch;
+                    index++;
                 }
             }
         }
-
-        // return length of compressed vector
-        return ans;
+        return index;
     }
 };
