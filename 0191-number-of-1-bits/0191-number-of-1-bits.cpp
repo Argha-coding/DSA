1ffed10b65fef6e7c3c6f43df31d35cd28d6f923
Argha Ghosh <ghoshargha777@gmail.com>
2026-06-09 16:12:22 +0530

    Time: 0 ms (100%), Space: 8.2 MB (47.16%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-     int count = 0;
-
-     while(n){
-        count+= (n%2);
-        n /=2;
-     }
-     return count;
+    return __builtin_popcount(n);
     }
 };
