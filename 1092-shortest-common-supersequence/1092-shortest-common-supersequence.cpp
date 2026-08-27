65882c0ad31f9d4e30866cc281f35baa14f4e215
Argha Ghosh <ghoshargha777@gmail.com>
2026-08-27 22:28:21 +0530

    Time: 30 ms (12.44%), Space: 26.9 MB (43.25%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,59 +1,51 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        // Step 1: Find the longest common subsequence using dynamic programming
-        int m = str1.length();
-        int n = str2.length();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        
-        // Fill the dp table
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    string shortestCommonSupersequence(string s1, string s2) {
+        int m = s1.length();
+        int n = s2.length();
+        vector<vector<int>> t(m + 1, vector<int> (n + 1));
+
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (i == 0 || j == 0) {
+                    t[i][j] = i + j;
+                } else  {
+                    if (s1[i - 1] == s2[j - 1]) {
+                        t[i][j] = 1 + t[i - 1][j - 1];
+                    } else {
+                        t[i][j] = 1 + min(t[i][j - 1], t[i - 1][j]);
+                    }
                 }
             }
         }
-        
-        // Step 2: Construct the shortest common supersequence
-        // Start from the bottom right of the dp table
-        int i = m, j = n;
         string result = "";
-        
+        int i = m, j = n;
         while (i > 0 && j > 0) {
-            if (str1[i - 1] == str2[j - 1]) {
-                // If the characters are the same, add it once
-                result.push_back(str1[i - 1]);
+            if (s1[i - 1] == s2[j - 1]) {
+                result.push_back(s1[i - 1]);
                 i--;
                 j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                // If coming from top has higher value, take character from str1
-                result.push_back(str1[i - 1]);
-                i--;
             } else {
-                // Otherwise, take character from str2
-                result.push_back(str2[j - 1]);
-                j--;
+                if (t[i - 1][j] < t[i][j - 1]) {
+                    result.push_back(s1[i - 1]);
+                    i--;
+
+                } else {
+                    result.push_back(s2[j - 1]);
+                    j--;
+                }
             }
         }
-        
-        // Add remaining characters from str1 (if any)
+
         while (i > 0) {
-            result.push_back(str1[i - 1]);
+            result.push_back(s1[i - 1]);
             i--;
         }
-        
-        // Add remaining characters from str2 (if any)
         while (j > 0) {
-            result.push_back(str2[j - 1]);
+            result.push_back(s2[j - 1]);
             j--;
         }
-        
-        // Reverse the result to get the final supersequence
-        reverse(result.begin(), result.end());
-        
+        reverse(result.begin() , result.end());
         return result;
     }
 };
