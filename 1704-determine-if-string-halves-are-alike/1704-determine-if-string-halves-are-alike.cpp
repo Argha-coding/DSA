20a01e5aa30918b367a8ba5cd3f3b0bbea8b97af
Argha Ghosh <ghoshargha777@gmail.com>
2026-09-04 20:23:12 +0530

    Time: 2 ms (17.73%), Space: 9.4 MB (22.14%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-    bool isVowel(char &ch){
-        return ( ch == 'a'|| ch =='e'|| ch=='i'||  ch=='o' || ch=='u'|| ch == 'A' || ch == 'E' ||ch == 'I' || ch=='O' || ch =='U');
-    }
+    
     bool halvesAreAlike(string s) {
         int n  = s.length();
         int mid = n / 2 ;
@@ -10,9 +8,26 @@ public:
         int j = mid;
         int countL = 0 ;
         int countR= 0;
+        unordered_set<char> st;
+        st.insert('a');
+        st.insert('e');
+        st.insert('i');
+        st.insert('o');
+        st.insert('u');
+        st.insert('A');
+        st.insert('E');
+        st.insert('I');
+        st.insert('O');
+        st.insert('U');
+
+
+
+
+
+
         while( i < n /2 && j < n){
-             if (isVowel(s[i])) countL++;
-            if (isVowel(s[j])) countR++;
+             if(st.find(s[i])  != st.end()) countL++;
+           if (st.find (s[j]) != st.end()) countR++;
             i++;
             j++;
 
