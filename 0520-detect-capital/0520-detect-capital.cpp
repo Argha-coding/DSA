a4d7400379f8076d73dd2d5282365ab24a9850dd
Argha Ghosh <ghoshargha777@gmail.com>
2026-09-11 11:29:23 +0530

    Time: 0 ms (100%), Space: 8 MB (79.8%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    bool check(string word , char start , char end){
-        for(char &ch : word){
-            if( ch < start || ch > end)
-                return false;
-        } 
-            return true;
-    }
-    
-     
     bool detectCapitalUse(string word) {
-        if(check(word ,'A' , 'Z') || check(word ,'a' , 'z') || check(word.substr(1),'a','z')){
-            return true;
+        int  capitalcounts = 0; 
 
+        for( char &ch : word){
+            if(isupper(ch))
+                capitalcounts++;
+
+
+        }
+        if( capitalcounts == 0 || capitalcounts == word.length() || capitalcounts ==1 && isupper(word[0])){
+            return true;
         }
         return false;
     }
